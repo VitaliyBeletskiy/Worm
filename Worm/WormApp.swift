6865689d88373c2cbc815117b90e7e63c7f16952
Vitaliy Beletskiy <vitaliy.beletskiy@gmail.com>
2020-11-19 22:12:03 +0100

    Initial Commit

---
+++ Worm/WormApp.swift
@@ -0,0 +1,17 @@
+//
+//  WormApp.swift
+//  Worm
+//
+//  Created by Vitaliy on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct WormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
